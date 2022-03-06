a9c7a56f836b55724b020b8a54a8e27a8a0fa375
Диана Ильина <diana.1993.30@gmail.com>
2022-03-06 17:51:37 +0000

    Git init miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1200;
+        int bonusMile = ticketPrice/20;
+
+        System.out.println("Количество бонусных милей:" + bonusMile);
+    }
+}
